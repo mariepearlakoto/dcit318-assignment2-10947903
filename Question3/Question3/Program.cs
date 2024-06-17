8881afff60b8eb837adacc2465d634dd5283301d
mariepearlakoto <mynakoto@st.ug.edu.gh>
2024-06-17 15:25:28 +0000

    interfaces done

--- Question3/Question3/Program.cs
+++ Question3/Question3/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,my World!");
+﻿
 
+public interface IMovable
+{
+    void Move();
+}
+
+public class Car : IMovable {
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable car1 = new Car();
+        IMovable bicycle1 = new Bicycle();
+
+        car1.Move();
+        bicycle1.Move();
+    }
+}
